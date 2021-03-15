8f707ac7c53fca38777e93bef5f20d5e88b1907b
Amir Eftekhari <79808812+AmirEfi@users.noreply.github.com>
2021-03-15 23:30:01 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String [] args){
+        System.out.println("Hello git");
+<<<<<<< HEAD
+        System.out.println("My name is: Amirreza Eftekhari");
+        System.out.println("My student ID is 99243014");
+>>>>>>> student-id
+    }
+}  
