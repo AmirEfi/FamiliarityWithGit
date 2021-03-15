2549c71dde5b6608185abae12a4a532c2240ce5c
Amir Eftekhari <79808812+AmirEfi@users.noreply.github.com>
2021-03-15 23:40:58 +0330

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String [] args){
-        System.out.println("Hello git");
-<<<<<<< HEAD
-        System.out.println("My name is: Amirreza Eftekhari");
-        System.out.println("My student ID is 99243014");
->>>>>>> student-id
-    }
-}  
